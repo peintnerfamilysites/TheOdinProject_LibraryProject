b5f82365356a3e71a43f0536f05ad486238458a1
peintnerfamilysites <peintnerfamilysites@gmail.com>
2026-06-21 14:06:54 -0500

    mostly functional need to add form functions

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function displayBooks(){
     clearBooks(library);
     myLibrary.forEach(book => {
         // Call the renderBook fn to create the html book passing it details.
-        const currentBook = renderBook(book.title,book.author,book.pages,book.read,book.id);
+        const currentBook = renderBook(book.title,book.author,book.pages,book.read,book.id,library);
         // Append to the page
         library.appendChild(currentBook);
     });
@@ -37,10 +37,48 @@ function clearBooks(library){
     return 'Cleared Books';
 }
 
-function renderBook(title,author,pages,read,id){
+function renderBook(t,a,p,r,id,l){
     // Create the elements for the book
     const bookCon = document.createElement('div');
-    
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const pages = document.createElement('div');
+    const read = document.createElement('div');
+    const del = document.createElement('button');
+    // Classes
+    bookCon.classList.add(`${id}`,'bookCon');
+    title.classList.add('title');
+    author.classList.add('author');
+    pages.classList.add('pages');
+    read.classList.add('read');
+    del.classList.add('delete');
+    // Content
+    title.textContent = `Title: ${t}`;
+    author.textContent = `Author: ${a}`;
+    pages.textContent = `Pages: ${p}`;
+    read.textContent = `Read: ${r}`;
+    del.textContent = 'Delete';
+    // Functionality
+    del.addEventListener('click',(e)=>{
+        e.preventDefault();
+        // get index
+        const index = myLibrary.findIndex(i => i.id === id);
+        console.log(index);
+        // splice the entry
+        myLibrary.splice(index,1);
+        // Clear and redisplay
+        clearBooks(l);
+        displayBooks();
+        return 'Book deleted and updated';
+    });
+    // append everything in proper order
+    bookCon.appendChild(title);
+    bookCon.appendChild(author);
+    bookCon.appendChild(pages);
+    bookCon.appendChild(read);
+    bookCon.appendChild(del);
+
+    return bookCon;
 }
 
 console.log(myLibrary);
